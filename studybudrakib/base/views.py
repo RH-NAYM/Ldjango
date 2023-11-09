14af9c718af0c9f67f0236fce97755b0ff302b64
MD Rakibul Hasan Naym <73577646+RH-NAYM@users.noreply.github.com>
2023-11-09 18:28:52 +0600

    check point save at 56 minutes

--- studybudrakib/base/views.py
+++ studybudrakib/base/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python!!!'},
+    {'id': 2, 'name': 'Lets Degign'},
+    {'id': 3, 'name': 'Developers'},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    context = {"rooms":rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
